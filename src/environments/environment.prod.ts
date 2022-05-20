b791bcd6c5767755efe8ee4e67630a0636a7c904
unknown <muriukijoel@gmail.com>
2022-05-20 16:55:12 +0300

    switch to mhasibu firestore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,13 +5,23 @@ export const environment = {
   log: false,
   environmentName: "production",
   firebase: {
-    apiKey: "AIzaSyBxmkNL3J8EBH2lrp0WFnQQ4Fv3936Q8UQ",
-    authDomain: "redmond-fire-dev.firebaseapp.com",
-    projectId: "redmond-fire-dev",
-    storageBucket: "redmond-fire-dev.appspot.com",
-    messagingSenderId: "520886838084",
-    appId: "1:520886838084:web:2a7f1e145a46875f412d11"
+    apiKey: "AIzaSyAIthaeYFbYu75ztCTCkgJdAiUHp-8JSbA",
+    authDomain: "mhasibufirestore.firebaseapp.com",
+    databaseURL: "https://mhasibufirestore.firebaseio.com",
+    projectId: "mhasibufirestore",
+    storageBucket: "mhasibufirestore.appspot.com",
+    messagingSenderId: "437301224513",
+    appId: "1:437301224513:web:1c6390b665ceb0e79e92c0",
+    measurementId: "G-HWPK0NXVC6"
   }
+  // firebase: {
+  //   apiKey: "AIzaSyBxmkNL3J8EBH2lrp0WFnQQ4Fv3936Q8UQ",
+  //   authDomain: "redmond-fire-dev.firebaseapp.com",
+  //   projectId: "redmond-fire-dev",
+  //   storageBucket: "redmond-fire-dev.appspot.com",
+  //   messagingSenderId: "520886838084",
+  //   appId: "1:520886838084:web:2a7f1e145a46875f412d11"
+  // }
   // firebase: {
   //     apiKey: "AIzaSyAv8ph2xGIAuVETzURt1yTGUNZ2Bh1m5dE",
   //     authDomain: "redmond-fire.firebaseapp.com",
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -4,11 +4,21 @@ export const environment = {
   admin: false,
   environmentName: "staging",
   firebase: {
-    apiKey: "AIzaSyBxmkNL3J8EBH2lrp0WFnQQ4Fv3936Q8UQ",
-    authDomain: "redmond-fire-dev.firebaseapp.com",
-    projectId: "redmond-fire-dev",
-    storageBucket: "redmond-fire-dev.appspot.com",
-    messagingSenderId: "520886838084",
-    appId: "1:520886838084:web:2a7f1e145a46875f412d11"
+    apiKey: "AIzaSyAIthaeYFbYu75ztCTCkgJdAiUHp-8JSbA",
+    authDomain: "mhasibufirestore.firebaseapp.com",
+    databaseURL: "https://mhasibufirestore.firebaseio.com",
+    projectId: "mhasibufirestore",
+    storageBucket: "mhasibufirestore.appspot.com",
+    messagingSenderId: "437301224513",
+    appId: "1:437301224513:web:1c6390b665ceb0e79e92c0",
+    measurementId: "G-HWPK0NXVC6"
   }
+  // firebase: {
+  //   apiKey: "AIzaSyBxmkNL3J8EBH2lrp0WFnQQ4Fv3936Q8UQ",
+  //   authDomain: "redmond-fire-dev.firebaseapp.com",
+  //   projectId: "redmond-fire-dev",
+  //   storageBucket: "redmond-fire-dev.appspot.com",
+  //   messagingSenderId: "520886838084",
+  //   appId: "1:520886838084:web:2a7f1e145a46875f412d11"
+  // }
 };
